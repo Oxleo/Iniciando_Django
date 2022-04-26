6ffae8aae0e37366f3a4ac8a3ec965727b2304a4
Oxleo <medeirosmarques@hotmail.com>
2022-04-26 09:44:39 -0300

    Projeto Inicial_Fundamentos

---
+++ Iniciando_Django/live_#1_django/usuario/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+import json
+from .models import Usuario 
+
+def cadastro(request):
+    return HttpResponse('você esta na pagina de cadastro')
+
+def login(request):
+    status = request.GET.get('status')
+    return render(request, 'login.html', {'status': status})
+
+def home(request):
+    return HttpResponse("Você esta na home do usuario")
+
+def salvar(request):
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    idade = request.POST.get('idade')
+    
+    try:
+        usuario = Usuario(
+            nome=nome,
+            sobrenome=sobrenome,
+            idade=idade
+        )
+        usuario.save()
+
+        return redirect('/usuario/login?status=1')
+    except:
+        return redirect('/usuario/login?status=2')
